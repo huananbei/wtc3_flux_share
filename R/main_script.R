9014d6794f79880307fff5ade92510957959c758
jedrake <je.drake@uws.edu.au>
2015-10-15 10:04:36 +1100

    renamed main script

--- R/main_script.R
+++ R/main_script.R
@@ -1,42 +1,66 @@
+#- required libraries
+library(HIEv)
+library(mgcv)
+library(scales)
+library(gplots)
+library(plotBy)
+library(plantecophys)
+library(magicaxis)
+library(lubridate)
+library(doBy)
+library(Hmisc)
+library(zoo)
 
+#- load the analysis and plotting functions that do all of the actual work
 source("R/functions.R")
 
-
-
+#-------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------
 #- plot R/A with a leaf-scale model (Figure 1)
 plotCUE_conceptual_fig(toexport=F,Tdew=10,Ca=400,Vcmax=100,Jmax=125,Tleaf=10:42,PPFD=1500)
 #-------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
+
+
+
 
 
 
 #-------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
 #- Plot respiration vs. temperature curves for leaves and whole-tree chambers (Figure 2)
 
-#- get the whole-canopy R vs. T datasets
+#- get and process the whole-canopy R vs. T datasets
 fits.list <- return_Rcanopy_closed()
 fits.mass <- fits.list[[1]]     #- tree-level data
 fits.trt <- fits.list[[2]]      #- treatment averages
 
-#- get the leaf-scale RvsT data
-rvt.c <- getRTWTC3()
 
 #- plot R vs. T (Figure 2)
 plotRvsT_figure2(fits.mass=fits.mass,fits.trt=fits.trt,export=F)
 #-------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
 
 
 
+
+
+#-------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------
 #- plot Rbranch and Rleaf measured at 15 degrees C (Figure 3)
 plotRleafRbranch(export=F)
 #-------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
+
+
+
 
 
 
 
 #-------------------------------------------------------------------------------------------------------------------
-#- Read and process the hourly flux dataset
+#-------------------------------------------------------------------------------------------------------------------
+#- Read and process the hourly flux dataset. Plot Figures 4 and 5.
 
 #- read in the hourly flux data
 dat.hr <- read.csv("data/WTC_TEMP_CM_WTCFLUX_20130910-20140530_L2_V1.csv")
@@ -50,10 +74,16 @@ dat.hr.p <- partitionHourlyFluxCUE_arr(dat.hr.gf=dat.hr,Ea=57.69,lagdates=3)
 #- plot an example week of partitioned fluxes
 plotPartitionedFluxes(dat.hr.gf3=dat.hr.p,ch_toplot="C07",startDate="2014-3-22",endDate="2014-3-27",write=F)
 
+#- get daily sums
+cue.list <- returnCUE.day(dat=dat.hr.p) # get daily sums from hourly data
+cue.day <- cue.list[[1]]                # extract chamber values on each day
+cue.day.trt <- cue.list[[2]]            # extract treatment averages
+
 #- plot met, Ra, GPP, and Ra/GPP data over time (Figure 4)
+plotPAR_AirT_CUE_GPP_Ra(cue.day.trt=cue.day.trt,export=F,lwidth=2.75)
 
+
+#- plot Figure 5
+plotGPP_Ra_CUE_metdrivers(cue.day=cue.day,export=F,shading=0.7)
 #-------------------------------------------------------------------------------------------------------------------
-
-
-#- get the whole-tree flux data
-downloadHIEv(searchHIEv("WTC_TEMP_CM_WTCFLUX_20130910-20140530_L2_V1"),topath="data/")
+#-------------------------------------------------------------------------------------------------------------------
