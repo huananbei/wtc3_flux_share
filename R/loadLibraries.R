73e36eda9fb193df18ed98106872e14f4068f319
jedrake <je.drake@uws.edu.au>
2015-10-22 11:10:57 +1100

    Fixed issues with "}" in loadLibraries.R

--- R/loadLibraries.R
+++ R/loadLibraries.R
@@ -1,44 +1,60 @@
 #- required libraries
+if (require("shape",quietly=T)==F) {
+  install.packages("shape")
+  library(shape,quietly=T)}
 
 if (require("mgcv",quietly=T)==F) {
   install.packages("mgcv")
   library(mgcv,quietly=T)}
+
 if (require("scales",quietly=T)==F) {
   install.packages("scales")
   library(scales,quietly=T)}
+
 if (require("gplots",quietly=T)==F) {
   install.packages("gplots")
   library(gplots,quietly=T)}
+
 if (require("magicaxis",quietly=T)==F) {
   install.packages("magicaxis")
   library(magicaxis,quietly=T)}
+
 if (require("lubridate",quietly=T)==F){
   install.packages("lubridate")
   library(lubridate,quietly=T)} 
+
 if (require("doBy",quietly=T)==F) {
   install.packages("doBy")
   library(doBy,quietly=T)}
+
 if (require("Hmisc",quietly=T)==F) {
   install.packages("Hmisc")
   library(Hmisc,quietly=T)}
+
 if (require("zoo",quietly=T)==F) {
   install.packages("zoo")
   library(zoo,quietly=T)}
+
 if (require("hexbin",quietly=T)==F) {
   install.packages("hexbin")
   library(hexbin,quietly=T)}
+
 if (require("nlme",quietly=T)==F) {
   install.packages("nlme")
   library(nlme,quietly=T)}
+
 if (require("lsmeans",quietly=T)==F) {
   install.packages("lsmeans")
   library(lsmeans,quietly=T)}
+
 if (require("car",quietly=T)==F) {
   install.packages("car")
   library(car,quietly=T)}
+
 if (require("data.table",quietly=T)==F) {
   install.packages("data.table")
   library(data.table,quietly=T)}
+
 if (require("calibrate",quietly=T)==F) {
   install.packages("calibrate")
   library(calibrate,quietly=T)}
